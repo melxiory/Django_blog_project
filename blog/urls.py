d97849506622d558163c795901d7314aaa9c3429
melxiory <sibagatullinnail@gmail.com>
2023-03-24 20:12:51 +0300

    Create abd update templates. Update forms.py, urls.py, views.py!!

--- blog/urls.py
+++ blog/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     path('', MainView.as_view(), name='main'),
     # path('blog/<slug>/', PostsView.as_view(), name='posts'),
     # path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
-    # path('signup/', SignUpView.as_view(), name='signup'),
-    # path('signin/', SignInView.as_view(), name='signin'),
-    # path('signout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='signout', ),
-    # path('contact/', FeedBackView.as_view(), name='contact'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signin/', SignInView.as_view(), name='signin'),
+    path('signout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='signout', ),
+    path('contact/', FeedBackView.as_view(), name='contact'),
     # path('contact/success/', SuccessView.as_view(), name='success'),
     # path('search/', SearchResultsView.as_view(), name='search_results'),
     # path('tag/<slug:slug>/', TagView.as_view(), name="tag"),
+    path('privacy-policy/', PrivacyPolicy.as_view(), name='privacy_policy')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
-# from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate
+from django.core.mail import send_mail
 # from django.core.mail import send_mail
 # from django.db.models import Q
-# from django.http import HttpResponseRedirect, BadHeaderError, HttpResponse
+from django.http import HttpResponseRedirect, BadHeaderError, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 # from django.core.paginator import Paginator
 # from taggit.models import Tag
 from blog.models import Profile, Category, Post, Comment
+from .forms import SignInForm, SigUpForm, FeedBackForm
 
 
 class MainView(View):
@@ -65,71 +67,74 @@ class MainView(View):
 #             'comment_form': comment_form
 #         })
 #
-#
-# class SignUpView(View):
-#     def get(self, request, *args, **kwargs):
-#         form = SigUpForm()
-#         return render(request, 'myblog/signup.html', context={
-#             'form': form,
-#         })
-#
-#     def post(self, request, *args, **kwargs):
-#         form = SigUpForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             if user is not None:
-#                 login(request, user)
-#                 return HttpResponseRedirect('/')
-#         return render(request, 'myblog/signup.html', context={
-#             'form': form,
-#         })
-#
-#
-# class SignInView(View):
-#     def get(self, request, *args, **kwargs):
-#         form = SignInForm()
-#         return render(request, 'myblog/signin.html', context={
-#             'form': form,
-#         })
-#
-#     def post(self, request, *args, **kwargs):
-#         form = SignInForm(request.POST)
-#         if form.is_valid():
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = authenticate(request, username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return HttpResponseRedirect('/')
-#         return render(request, 'myblog/signin.html', context={
-#             'form': form,
-#         })
-#
-#
-# class FeedBackView(View):
-#     def get(self, request, *args, **kwargs):
-#         form = FeedBackForm()
-#         return render(request, 'myblog/contact.html', context={
-#             'form': form,
-#             'title': 'Написать мне'
-#         })
-#
-#     def post(self, request, *args, **kwargs):
-#         form = FeedBackForm(request.POST)
-#         if form.is_valid():
-#             name = form.cleaned_data['name']
-#             from_email = form.cleaned_data['email']
-#             subject = form.cleaned_data['subject']
-#             message = form.cleaned_data['message']
-#             try:
-#                 send_mail(f'От {name} | {subject}', message, from_email, ['sibagatullinnail@gmail.com'])
-#             except BadHeaderError:
-#                 return HttpResponse('Невалидный заголовок')
-#             return HttpResponseRedirect('success')
-#         return render(request, 'myblog/contact.html', context={
-#             'form': form,
-#         })
-#
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SigUpForm()
+        return render(request, 'blog/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SigUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'blog/signup.html', context={
+            'form': form,
+        })
+
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignInForm()
+        return render(request, 'blog/signin.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'blog/signin.html', context={
+            'form': form,
+        })
+
+
+class PrivacyPolicy(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'blog/privacy_policy.html')
+
+class FeedBackView(View):
+    def get(self, request, *args, **kwargs):
+        form = FeedBackForm()
+        return render(request, 'blog/contact.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            full_name = form.cleaned_data['full_name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(f'От {full_name} | {subject}', message, from_email, ['sibagatullinnail@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Невалидный заголовок')
+            return HttpResponseRedirect('success')
+        return render(request, 'blog/contact.html', context={
+            'form': form,
+        })
+
 #
 # class SuccessView(View):
 #     def get(self, request, *args, **kwargs):
