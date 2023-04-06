3bff0afdb08818ca8e935e298487ec9f0ca3303d
melxiory <sibagatullinnail@gmail.com>
2023-04-06 19:31:38 +0300

    Create and update templates. Update views.py, urls.py, forms.py...!

--- Django_blog_project/settings.py
+++ Django_blog_project/settings.py
@@ -172,7 +172,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sibagatullinnail@gmail.com'
-EMAIL_HOST_PASSWORD = 'uhiufbdclcabemim'
+EMAIL_HOST_PASSWORD = ''
 ###################################
 
 COMMENTS_APP = 'threadedcomments'
